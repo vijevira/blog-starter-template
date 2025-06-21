a13e49d6984ec7ac176388bb73aeb88725993304
vijevira <vijevira@gmail.com>
2025-06-21 20:35:28 +0530

    blog to family

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Endra Family";
+export const SITE_DESCRIPTION = "Welcome to Endra family website!";
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,9 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  // Load Markdown and MDX files in the `src/content/blog/` directory.
-  loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
+const family = defineCollection({
+  // Load Markdown and MDX files in the `src/content/family/` directory.
+  loader: glob({ base: "./src/content/family", pattern: "**/*.{md,mdx}" }),
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
@@ -15,4 +15,4 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+export const collections = { family };
